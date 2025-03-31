fd1200b7d7f6dd481dd98cc97e15bd4fc84bfc42
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-03-31 15:46:49 +0200

    Update podman-run-angular.bash

--- podman-run-angular.bash
+++ podman-run-angular.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t angular:demo .
-podman run --interactive --tty -p 4200:4200 angular:demo
+podman run --interactive --tty -p 4200:80 angular:demo
 echo "browse http://localhost:4200/?username=test"
